28d200abf38606719d3fe530c81931df3318ab6b
ro.mancilla <134629433+ro-mancilla@users.noreply.github.com>
2025-05-07 14:17:59 -0400

    Add files via upload

---
+++ src/main/java/com/flyaway/project/flyaway/FlyawayApplication.java
@@ -0,0 +1,13 @@
+package com.flyaway.project.flyaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyawayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlyawayApplication.class, args);
+	}
+
+}
